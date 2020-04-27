80ce443fd89e909bd06efd124e8dc33331eae6c7
frikilax <bertintheo@orange.fr>
2020-04-27 16:34:23 +0200

    TANOMALY:: start detection thread conditionally
    - new filter parameter 'start_detection_thread' to force activation of detection thread
    - detection thread started when filter instance is connected to a redis master through an unix socket

--- samples/ftanomaly/Generator.cpp
+++ samples/ftanomaly/Generator.cpp
@@ -25,7 +25,8 @@ bool Generator::LoadConfig(const rapidjson::Document &configuration) {
 
     std::string redis_alerts_channel; //the channel on which to publish alerts when detected
     std::string redis_alerts_list; //the list on which to add alerts when detected
-    unsigned int detection_frequency = 300; //detection thread launching frequence in seconds
+    bool start_detection_thread = false;  //whether to start the anomaly thread
+    unsigned int detection_frequency = 300; //detection thread launching frequency in seconds
 
     if(not configuration.HasMember("redis_socket_path")) {
         DARWIN_LOG_CRITICAL("TAnomaly::Generator:: \"redis_socket_path\" parameter missing, mandatory");
@@ -47,9 +48,22 @@ bool Generator::LoadConfig(const rapidjson::Document &configuration) {
         return false;
     }
 
+    // start detection thread if the local redis server is a master
+    if(darwin::toolkit::RedisManager::TestIsMaster(redis_socket_path)) {
+        DARWIN_LOG_INFO("TAnomaly::Generator:: direct connection to redis master found, detection thread will be started");
+        start_detection_thread = true;
+    }
+
+    // Override start_detection_thread with parameter
+    if(configuration.HasMember("start_detection_thread") and configuration["start_detection_thread"].IsBool()) {
+        start_detection_thread = configuration["detect"].GetBool();
+        DARWIN_LOG_INFO("TAnomaly:: Generator:: \"start_detection_thread\" specified, "
+                        + std::string((start_detection_thread ? "enabling" : "disabling")) + " detection");
+    }
+
     if(configuration.HasMember("detection_frequency") and configuration["detection_frequency"].IsInt()) {
         detection_frequency = configuration["detection_frequency"].GetUint64();
-        DARWIN_LOG_INFO("TAnomaly:: Generator:: detection frequence manually set to "
+        DARWIN_LOG_INFO("TAnomaly:: Generator:: detection frequency manually set to "
                             + std::to_string(detection_frequency));
     }
 
@@ -121,10 +135,20 @@ bool Generator::LoadConfig(const rapidjson::Document &configuration) {
         DARWIN_LOG_INFO("TAnomaly::Generator:: Log file configured");
     }
 
-    _anomaly_thread_manager = std::make_shared<AnomalyThreadManager>(_redis_internal, _log_file, redis_alerts_channel, redis_alerts_list);
-    if(!_anomaly_thread_manager->Start(detection_frequency)) {
-        DARWIN_LOG_CRITICAL("TAnomaly:: Generator:: Error when starting polling thread");
-        return false;
+    /* Only start the thread if
+    -> the thread is connected to master via unix socket (filter is on the master's machine)
+    -> or user forced it
+    */
+    if(start_detection_thread) {
+        _anomaly_thread_manager = std::make_shared<AnomalyThreadManager>(_redis_internal, _log_file, redis_alerts_channel, redis_alerts_list);
+        if(!_anomaly_thread_manager->Start(detection_frequency)) {
+            DARWIN_LOG_CRITICAL("TAnomaly:: Generator:: Error when starting polling thread");
+            return false;
+        }
+    }
+    else {
+        DARWIN_LOG_INFO("TAnomaly:: Generator:: detection thread is disabled (or no direct connection to redis master"
+         " was found) so detection will not start");
     }
 
     return true;
