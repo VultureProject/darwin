9714f0f05ca0c632df94a581718a806439104f2d
frikilax <bertintheo@orange.fr>
2020-05-12 11:26:34 +0200

    CMAKE:: add module to find yara, improve hiredis module to respect cmake best practices, use new yara target for content_inspection

--- cmake/FindHiredis.cmake
+++ cmake/FindHiredis.cmake
@@ -1,35 +1,88 @@
-# Find hiredis library and includes
-# This module defines
-#  HIREDIS_LIBRARIES, the libraries needed to use hiredis
-#  HIREDIS_INCLUDE_DIRS, the folders containing hiredis development files
-#  HIREDIS_FOUND, whether hiredis was found on system
+#[=======================================================================[.rst:
+FindHiredis
+-------
 
-set(HIREDIS_NAMES ${HIREDIS_NAMES} libhiredis.a hiredis)
+Finds the Hiredis library.
 
-# Try with manually given path
-find_library(
-  HIREDIS_LIBRARY
-  NAMES ${HIREDIS_NAMES}
-  HINTS ${HIREDIS_ROOT}
-  PATH_SUFFIXES lib/)
+Imported Targets
+^^^^^^^^^^^^^^^^
 
-find_path(
-  HIREDIS_INCLUDE_DIR
-  NAMES hiredis/hiredis.h
-  HINTS ${HIREDIS_ROOT}
-  PATH_SUFFIXES include/)
+This module provides the following imported targets, if found:
 
-if(HIREDIS_LIBRARY)
-  set(HIREDIS_FOUND "YES")
-else()
-  set(HIREDIS_FOUND "NO")
+``Hiredis::Hiredis``
+  The Hiredis library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Hiredis_FOUND``
+  True if the system has the Hiredis library.
+``Hiredis_VERSION``
+  The version of the Hiredis library which was found.
+``Hiredis_INCLUDE_DIRS``
+  Include directories needed to use Hiredis.
+``Hiredis_LIBRARIES``
+  Libraries needed to link to Hiredis.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Hiredis_INCLUDE_DIR``
+  The directory containing ``foo.h``.
+``Hiredis_LIBRARY``
+  The path to the Hiredis library.
+
+#]=======================================================================]
+
+find_package(PkgConfig)
+pkg_check_modules(PC_Hiredis QUIET hiredis)
+
+
+find_path(Hiredis_INCLUDE_DIR
+  NAMES hiredis.h
+  PATHS ${PC_Hiredis_INCLUDE_DIRS}
+  PATH_SUFFIXES hiredis
+)
+find_library(Hiredis_LIBRARY
+  NAMES libhiredis.a hiredis
+  PATHS ${PC_Hiredis_LIBRARY_DIRS}
+)
+
+set(Hiredis_VERSION ${PC_Hiredis_VERSION})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+	Hiredis
+  FOUND_VAR Hiredis_FOUND
+  REQUIRED_VARS
+    Hiredis_LIBRARY
+    Hiredis_INCLUDE_DIR
+  VERSION_VAR Hiredis_VERSION
+)
+
+if(Hiredis_FOUND)
+  set(Hiredis_LIBRARIES ${Hiredis_LIBRARY})
+  set(Hiredis_INCLUDE_DIRS ${Hiredis_INCLUDE_DIR})
+  set(Hiredis_DEFINITIONS ${PC_Hiredis_CFLAGS_OTHER})
 endif()
 
-set(HIREDIS_INCLUDE_DIRS ${HIREDIS_INCLUDE_DIR})
-set(HIREDIS_LIBRARIES ${HIREDIS_LIBRARY})
+if(Hiredis_FOUND AND NOT TARGET Hiredis::Hiredis)
+  add_library(Hiredis::Hiredis UNKNOWN IMPORTED)
+  set_target_properties(Hiredis::Hiredis PROPERTIES
+    IMPORTED_LOCATION "${Hiredis_LIBRARY}"
+    INTERFACE_COMPILE_OPTIONS "${PC_Hiredis_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${Hiredis_INCLUDE_DIR}"
+  )
+endif()
 
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  hiredis DEFAULT_MSG HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
+mark_as_advanced(
+  Hiredis_INCLUDE_DIR
+  Hiredis_LIBRARY
+)
 
-mark_as_advanced(HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
+# compatibility variables
+set(Hiredis_VERSION_STRING ${Hiredis_VERSION})
