8987724755a79b1b0373ddf84f657fdb9ffa8e8e
Hugo Soszynski <hugo.soszynski@outlook.com>
2020-05-13 16:32:48 +0200

    BREAKING HOSTLOOKUP UPDATE
    Updated fhostlookup to use a more versatile db format.
    New format provide the feed name and a optional score by entry:
     - The "feed_name" field is mandatory
     - The "data" field is mandatory
     - If no good entry is found in the data field the the filter is stopped
     -  The "entry" field is mandatory
     - The "score" field is optional and value should be between 0 and 100.
        If not value is default (100)
    
    Also updated the alert raised to include the feed name and changed
    the "host" field for "entry" to transition to a more generic filter.
    
    Added more fail tests on the db format and
    some check mixing good and malicious entries.

--- tests/filters/fhostlookup.py
+++ tests/filters/fhostlookup.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import json
 
 from tools.filter import Filter
 from tools.output import print_result
@@ -7,7 +8,7 @@ from darwin import DarwinApi
 
 class HostLookup(Filter):
     def __init__(self):
-        super().__init__(filter_name="hostlookup")
+        super().__init__(filter_name="hostlookup", thresold=80)
         self.database = "/tmp/database.txt".format(self.filter_name)
 
     def configure(self):
@@ -18,8 +19,7 @@ class HostLookup(Filter):
 
     def init_data(self, data):
         with open(self.database, mode='w') as file:
-            for d in data:
-                file.write(d+"\n")
+            file.write(json.dumps(data))
 
     def clean_files(self):
         super(HostLookup, self).clean_files()
@@ -32,19 +32,31 @@ class HostLookup(Filter):
 
 def run():
     tests = [
-        unique_untrusted_host_test,
-        unique_trusted_host_test,
-        multiple_trusted_host_test,
-        multiple_untrusted_host_test,
-        multiple_host_test,
-        init_data_test
+        db_no_feed_name,
+        db_wrong_feed_name_type,
+        db_no_data_field,
+        db_wrong_data_type,
+        db_empty_data,
+        db_no_entry_field,
+        db_no_usable_entry_field,
+        db_worng_entry_type,
+        exec_one_good,
+        exec_one_bad,
+        exec_one_bad_no_score,
+        exec_one_bad_score_lt_0,
+        exec_one_bad_score_gt_100,
+        exec_one_bad_wrong_score_type,
+        exec_multiple_good,
+        exec_multiple_bad,
+        exec_one_bad_multiple_good,
+        exec_multiple_bad_multiple_good
     ]
 
     for i in tests:
         print_result("hostlookup: " + i.__name__, i)
 
 
-def test(test_name, init_data, data, expected_certitudes, expected_certitudes_size):
+def test(test_name, init_data, data, expected_certitudes):
     ret = True
 
     # CONFIG
@@ -75,10 +87,10 @@ def test(test_name, init_data, data, expected_certitudes, expected_certitudes_si
         logging.error("Hostlookup Test : {} : No certitude list found in result".format(test_name))
 
 
-    if len(certitudes) != expected_certitudes_size:
+    if len(certitudes) != len(expected_certitudes):
         ret = False
         logging.error("HostLookup Test : {} : Unexpected certitude size of {} instead of {}"
-                      .format(test_name, len(certitudes), expected_certitudes_size))
+                      .format(test_name, len(certitudes), len(expected_certitudes)))
 
     if certitudes != expected_certitudes:
         ret = False
@@ -97,110 +109,328 @@ def test(test_name, init_data, data, expected_certitudes, expected_certitudes_si
     return ret
 
 
-"""
-We give an untrusted host
-"""
-def unique_untrusted_host_test():
-    return test(
-        "unique_untrusted_host_test",
-        [
-         "good_host_1",
-         "good_host_2",
-         "good_host_3",
-         ],
-        [["very_bad_host"]],
-        [0],1)
+def db_no_feed_name():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "data": [
+            {"entry": "bad_host_1", "score": 84},
+            {"entry": "bad_host_2", "score": 42},
+            {"entry": "bad_host_3", "score": 100},
+        ]
+    })
+    hostlookup_filter.configure()
 
-"""
-We give a trusted host
-"""
-def unique_trusted_host_test():
-    return test(
-        "unique_trusted_host_test",
-        [
-         "good_host_1",
-         "good_host_2",
-         "good_host_3",
-         ],
-        [["good_host_1"]],[100],1)
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
 
-"""
-We give a trusted host
-"""
 
-def multiple_trusted_host_test():
-    return test(
-        "multiple_trusted_host_test",
-        [
-         "good_host_1",
-         "good_host_2",
-         "good_host_3",
-         ],
-        [
-            ["good_host_3"],
-            ["good_host_1"],
-        ],
-        [100,100],2)
+def db_wrong_feed_name_type():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": 42,
+        "data": [
+            {"entry": "bad_host_1", "score": 84},
+            {"entry": "bad_host_2", "score": 42},
+            {"entry": "bad_host_3", "score": 100},
+        ]
+    })
+    hostlookup_filter.configure()
 
-"""
-We give multiple untrusted hosts
-"""
-def multiple_untrusted_host_test():
-    return test(
-        "multiple_untrusted_host_test",
-        [
-         "good_host_1",
-         "good_host_2",
-         "good_host_3",
-         ],
-        [
-            ["very_bad_host"],
-            ["even_more_very_bad_host"],
-            ["sneaky_host"],
-        ],
-        [0,0,0],3)
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
 
-"""
-We give multiple hosts
-"""
-def multiple_host_test():
-    return test(
-        "multiple_host_test",
-        [
-         "good_host_1",
-         "good_host_2",
-         "good_host_3",
-         ],
-        [
-            ["very_bad_host"],
-            ["even_more_very_bad_host"],
-            ["good_host_2"],
-            ["sneaky_host"],
-            ["good_host_1"],
-        ],
-        [0,0,100,0,100],5)
 
-"""
-We give database file with differents endlines, 
-blank lines, blank spaces and tabs
-"""
-def init_data_test():
+def db_no_data_field():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
+
+
+def db_wrong_data_type():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+        "data": "It's Supposed To Be Automatic But Actually You Have To Press This Button. John Brunner, Stand on Zanzibar."
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
+
+
+def db_empty_data():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+        "data": []
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
+
+
+def db_no_entry_field():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+        "data": [
+            {"score": 84},
+            {"entry": "bad_host_2", "score": 42},
+            {"entry": "bad_host_3", "score": 100},
+        ]
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return False
+    return True
+
+
+def db_no_usable_entry_field():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+        "data": [
+            {"score": 84},
+            {"score": 42},
+            {"score": 100},
+        ]
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return True
+    return False
+
+
+def db_worng_entry_type():
+    # CONFIG
+    hostlookup_filter = HostLookup()
+    # All the trusted hosts
+    hostlookup_filter.init_data({
+        "feed_name": "test-feed",
+        "data": [
+            {"entry": 42, "score": 84},
+            {"entry": "bad_host_2", "score": 42},
+            {"entry": "bad_host_3", "score": 100},
+        ]
+    })
+    hostlookup_filter.configure()
+
+    # START FILTER
+    if not hostlookup_filter.valgrind_start():
+        return False
+    return True
+
+
+def exec_one_good():
     return test(
-        "init_data_test",
-        [
-         "good_host_1\r",
-         "\tgood_host_2",
-         "  good_host_3  ",
-         "  ",
-         "",
-         "good_host_4",
-         ""
-         ],
+        "exec_one_good",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
+        [["good_host"]],
+        [0]
+    )
+
+
+def exec_one_bad():
+    return test(
+        "exec_one_bad",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
+        [["bad_host_2"]],
+        [42]
+    )
+
+
+def exec_one_bad_no_score():
+    return test(
+        "exec_one_bad_no_score",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3"},
+            ]
+        },
+        [["bad_host_3"]],
+        [100]
+    )
+
+
+def exec_one_bad_score_lt_0():
+    return test(
+        "exec_one_bad_score_lt_0",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": -42},
+            ]
+        },
+        [["bad_host_3"]],
+        [100]
+    )
+
+
+def exec_one_bad_score_gt_100():
+    return test(
+        "exec_one_bad_score_gt_100",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 1337},
+            ]
+        },
+        [["bad_host_3"]],
+        [100]
+    )
+
+
+def exec_one_bad_wrong_score_type():
+    return test(
+        "exec_one_bad_wrong_score_type",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": "When done well, software is invisible. Bjarne Stroustrup."},
+            ]
+        },
+        [["bad_host_3"]],
+        [100]
+    )
+
+
+def exec_multiple_good():
+    return test(
+        "exec_multiple_good",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
         [
             ["good_host_1"],
             ["good_host_2"],
             ["good_host_3"],
             ["good_host_4"],
-            [""],
         ],
-        [100,100,100,100,0],5)
+        [0, 0, 0, 0]
+    )
+
+
+def exec_multiple_bad():
+    return test(
+        "exec_multiple_bad",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
+        [
+            ["bad_host_2"],
+            ["bad_host_1"],
+            ["bad_host_2"],
+            ["bad_host_3"],
+        ],
+        [42, 84, 42, 100]
+    )
+
+
+def exec_one_bad_multiple_good():
+    return test(
+        "exec_one_bad_multiple_good",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
+        [
+            ["good_host_1"],
+            ["good_host_2"],
+            ["bad_host_2"],
+            ["good_host_3"],
+        ],
+        [0, 0, 42, 0]
+    )
+
+
+def exec_multiple_bad_multiple_good():
+    return test(
+        "exec_multiple_bad_multiple_good",
+        {
+            "feed_name": "test-feed",
+            "data": [
+                {"entry": "bad_host_1", "score": 84},
+                {"entry": "bad_host_2", "score": 42},
+                {"entry": "bad_host_3", "score": 100},
+            ]
+        },
+        [
+            ["good_host_1"],
+            ["bad_host_2"],
+            ["good_host_2"],
+            ["bad_host_1"],
+            ["good_host_3"],
+        ],
+        [0, 42, 0, 84, 0]
+    )
