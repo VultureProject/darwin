fe1d9f8301e6cb360f42648f5b0907722af84fb5
frikilax <bertintheo@orange.fr>
2020-06-12 16:26:45 +0000

    FYARA::Tests:: remove old fyarascan tests and update yara tests for new inputs format

--- tests/filters/fyara.py
+++ tests/filters/fyara.py
@@ -65,7 +65,7 @@ No rule file in configuration
 """
 def no_rule_files():
     yara_filter = Yara()
-    yara_filter.configure(ruleFileList="[]")
+    yara_filter.configure()
 
     if yara_filter.valgrind_start():
         logging.error("no_rule_files: filter starter without any rule file")
@@ -250,27 +250,27 @@ def input_with_several_rules(data, expected_certitudes=[0]):
 A rule file with several rules, a base 16 encoded input, no match
 """
 def hex_input_several_rules_no_match():
-    return input_with_several_rules(data=[["hex", "54686973207265706f72742c206279206974732076657279206c656e6774682c20646566656e647320697473656c6620616761696e737420746865207269736b206f66206265696e6720726561642e0a57696e73746f6e20436875726368696c6c0a"]])
+    return input_with_several_rules(data=[["54686973207265706f72742c206279206974732076657279206c656e6774682c20646566656e647320697473656c6620616761696e737420746865207269736b206f66206265696e6720726561642e0a57696e73746f6e20436875726368696c6c0a", "hex"]])
 
 
 """
 A rule file with several rules, a base 16 encoded input, a match
 """
 def hex_input_several_rules_one_match():
-    return input_with_several_rules(data=[["hex", "546f20646f20612067726561742044617277696e20646f2061206c6974746c652077726f6e672e0a286e6f74292057696c6c69616d205368616b65737065617265"]], expected_certitudes=[100])
+    return input_with_several_rules(data=[["546f20646f20612067726561742044617277696e20646f2061206c6974746c652077726f6e672e0a286e6f74292057696c6c69616d205368616b65737065617265", "hex"]], expected_certitudes=[100])
 
 """
 A rule file with several rules, 2 base 16 encoded inputs, 2 matches
 """
 def two_hex_inputs_several_rules_two_matches():
-    return input_with_several_rules(data=[["hex", "546f20646f20612067726561742044617277696e20646f2061206c6974746c652077726f6e672e0a286e6f74292057696c6c69616d205368616b65737065617265"],
-                                    ["hex", "6e69777261442c206e69777261442c206e6977726144"]], expected_certitudes=[100, 100])
+    return input_with_several_rules(data=[["546f20646f20612067726561742044617277696e20646f2061206c6974746c652077726f6e672e0a286e6f74292057696c6c69616d205368616b65737065617265", "hex"],
+                                    ["6e69777261442c206e69777261442c206e6977726144", "hex"]], expected_certitudes=[100, 100])
 
 """
 A rule file with several rules, a wrong base 16 encoded input, no match
 """
 def hex_input_several_rules_wrong_data():
-    return input_with_several_rules(data=[["hex", "54686973207265706f72742c206279206974732076657279206c656e6774682c20646566656e647320697473656c6620616761696e737420746865207269736b206f66206265696e6720726561642e0a57696e73746f6e20436875726368696c6czz"]], expected_certitudes=[101])
+    return input_with_several_rules(data=[["54686973207265706f72742c206279206974732076657279206c656e6774682c20646566656e647320697473656c6620616761696e737420746865207269736b206f66206265696e6720726561642e0a57696e73746f6e20436875726368696c6czz", "hex"]], expected_certitudes=[101])
 
 
 
@@ -279,27 +279,27 @@ def hex_input_several_rules_wrong_data():
 A rule file with several rules, a base 64 encoded input, no match
 """
 def base64_input_several_rules_no_match():
-    return input_with_several_rules(data=[["base64", "SSBkbyBub3QgZmVhciBjb21wdXRlcnMuIEkgZmVhciB0aGUgbGFjayBvZiB0aGVtLgpJc2FhYyBBc2ltb3Y="]])
+    return input_with_several_rules(data=[["SSBkbyBub3QgZmVhciBjb21wdXRlcnMuIEkgZmVhciB0aGUgbGFjayBvZiB0aGVtLgpJc2FhYyBBc2ltb3Y=", "base64"]])
 
 
 """
 A rule file with several rules, a base 64 encoded input, a match
 """
 def base64_input_several_rules_one_match():
-    return input_with_several_rules(data=[["base64", "QXMgRGFyd2luIGhpbXNlbGYgd2FzIGF0IHBhaW5zIHRvIHBvaW50IG91dCwgbmF0dXJhbCBzZWxlY3Rpb24gaXMgYWxsIGFib3V0IGRpZmZlcmVudGlhbCBzdXJ2aXZhbCB3aXRoaW4gc3BlY2llcywgbm90IGJldHdlZW4gdGhlbS4KUmljaGFyZCBEYXdraW5z"]], expected_certitudes=[100])
+    return input_with_several_rules(data=[["QXMgRGFyd2luIGhpbXNlbGYgd2FzIGF0IHBhaW5zIHRvIHBvaW50IG91dCwgbmF0dXJhbCBzZWxlY3Rpb24gaXMgYWxsIGFib3V0IGRpZmZlcmVudGlhbCBzdXJ2aXZhbCB3aXRoaW4gc3BlY2llcywgbm90IGJldHdlZW4gdGhlbS4KUmljaGFyZCBEYXdraW5z", "base64"]], expected_certitudes=[100])
 
 """
 A rule file with several rules, 2 base 64 encoded inputs, 2 matches
 """
 def two_base64_inputs_several_rules_two_matches():
-    return input_with_several_rules(data=[["base64", "QXMgRGFyd2luIGhpbXNlbGYgd2FzIGF0IHBhaW5zIHRvIHBvaW50IG91dCwgbmF0dXJhbCBzZWxlY3Rpb24gaXMgYWxsIGFib3V0IGRpZmZlcmVudGlhbCBzdXJ2aXZhbCB3aXRoaW4gc3BlY2llcywgbm90IGJldHdlZW4gdGhlbS4KUmljaGFyZCBEYXdraW5z"],
-                                    ["base64", "bml3cmFELCBuaXdyYUQgbml3cmFELi4u"]], expected_certitudes=[100, 100])
+    return input_with_several_rules(data=[["QXMgRGFyd2luIGhpbXNlbGYgd2FzIGF0IHBhaW5zIHRvIHBvaW50IG91dCwgbmF0dXJhbCBzZWxlY3Rpb24gaXMgYWxsIGFib3V0IGRpZmZlcmVudGlhbCBzdXJ2aXZhbCB3aXRoaW4gc3BlY2llcywgbm90IGJldHdlZW4gdGhlbS4KUmljaGFyZCBEYXdraW5z", "base64"],
+                                    ["bml3cmFELCBuaXdyYUQgbml3cmFELi4u", "base64"]], expected_certitudes=[100, 100])
 
 """
 A rule file with several rules, a wrong base 64 encoded input, no match
 """
 def base64_input_several_rules_wrong_data():
-    return input_with_several_rules(data=[["base64", "TXkgdGhlb3J5IG9mIGV2b2x1dGlvbiBpcyB0aGF0IERhcndpbiB3YXMgYWRvcHRlZC4KU3RldmVuIFdyaWdodAo"]], expected_certitudes=[101])
+    return input_with_several_rules(data=[["TXkgdGhlb3J5IG9mIGV2b2x1dGlvbiBpcyB0aGF0IERhcndpbiB3YXMgYWRvcHRlZC4KU3RldmVuIFdyaWdodAo", "base64"]], expected_certitudes=[101])
 
 
 
@@ -331,12 +331,12 @@ def two_plain_inputs_several_rules_two_matches():
 A rule file with several rules, several inputs with different encodings, some matches
 """
 def many_inputs_several_rules_different_results():
-    return input_with_several_rules(data=[  ["hex", "7468697320776f6e2774206d61746368"],
-                                            ["hex", "44617277696e2069732074686520616e73776572"],
-                                            ["hex", "00112233445566778899aabbccddeeffoups"],
-                                            ["base64", "SXQncyBhIHRyYXAgIQ= ="],
-                                            ["base64", "VGhpcyB3b24ndCBtYXRjaCBlaXRoZXI="],
-                                            ["base64", "bml3cmFEIGlzIGp1c3QgdGhlIHJlZmxleGlvbiBvZiBEYXJ3aW4u"],
+    return input_with_several_rules(data=[  ["7468697320776f6e2774206d61746368", "hex"],
+                                            ["44617277696e2069732074686520616e73776572", "hex"],
+                                            ["00112233445566778899aabbccddeeffoups", "hex"],
+                                            ["SXQncyBhIHRyYXAgIQ= =", "base64"],
+                                            ["VGhpcyB3b24ndCBtYXRjaCBlaXRoZXI=", "base64"],
+                                            ["bml3cmFEIGlzIGp1c3QgdGhlIHJlZmxleGlvbiBvZiBEYXJ3aW4u", "base64"],
                                             [""],
                                             ["this is justDarwin matching"],
                                             ], expected_certitudes=[0, 100, 101, 101, 0, 100, 0, 100])
