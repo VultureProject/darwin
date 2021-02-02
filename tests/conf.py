27b10444d828183d1fa26d8eb9b2c566be29c0ec
frikilax <theo.bertin@advens.fr>
2021-02-02 16:48:15 +0100

    TESTS:: SIMPLIFY CONF.PY
    - remove MANAGER_SOCKET_PATH, FILTER_SOCKETS_DIR, FILTER_PIDS_DIR and DEFAULT_CONFIGURATION_PATH
    - put a single parameter TEST_FILES_DIR instead to set as root for all temporary files during tests (missing darwin.log)

--- tests/conf.py
+++ tests/conf.py
@@ -1,14 +1,10 @@
-# DARWIN FILES
-MANAGEMENT_SOCKET_PATH = '/var/sockets/darwin/darwin.sock'
-FILTER_SOCKETS_DIR = "/var/sockets/darwin/"
-FILTER_PIDS_DIR = "/var/run/darwin/"
-DEFAULT_CONFIGURATION_PATH = '/tmp/darwin.conf'
+# Where all Runtime and temporary files will be put (configurations, sockets, pid files, logging files... except darwin.log)
+TEST_FILES_DIR = '/tmp'
 
 # ENV CONFIG
 PYTHON_ENV_PATH = "/usr/local/lib/python3.7"
 DEFAULT_PYTHON_EXEC = 'python3'
 
-TEST_FILES_DIR = '/tmp'
 DEFAULT_MANAGER_PATH = '/home/darwin/manager/manager.py'
 DEFAULT_FILTER_PATH = '/home/darwin/filters/'
 
