7c7df2f42c3c5589d91d0fef3b9ee966627de151
Theo BERTIN <theo.bertin@advens.fr>
2021-12-24 18:41:29 +0100

    TESTS::ADDED:: test Manager writting to logfile and respecting logrotation signal

--- tests/manager_socket/test.py
+++ tests/manager_socket/test.py
@@ -2,6 +2,7 @@ import pprint
 import manager_socket.monitor_test as monitor_test
 import manager_socket.update_test as update_test
 import manager_socket.reporting_test as reporting_test
+import manager_socket.logging_test as logging_test
 from tools.output import print_results
 
 def run():
@@ -10,6 +11,7 @@ def run():
     monitor_test.run()
     update_test.run()
     reporting_test.run()
+    logging_test.run()
 
     print()
     print()
